d77bb1936a76def11c30fb6f2681528d5f9e190a
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-05-16 15:00:15 +0200

    created the Home component adn imported searchForm and movies to it

---
+++ imdb-movies/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Form from './SearchFrom'
+import Movies from './Movies'
+const Home = () => {
+  return (
+    <main>
+      <Form />
+      <Movies />
+    </main>
+  )
+}
+
+export default Home
